f2d75cef6c07869a49a982c813211847fcf2c849
giovanni-suriani <giovanni.suriani2@gmail.com>
2025-06-13 10:58:14 -0300

    1st comm

--- Pratica_Jorge/P02/Part1rev0/simulation/modelsim/rtl_work/decodificador/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity decodificador is
-    port(
-        SW              : in     vl_logic_vector(3 downto 0);
-        DISPLAY         : out    vl_logic_vector(0 to 6)
-    );
-end decodificador;
--- Pratica_Ref2/registrador1b.v
+++
@@ -1,9 +0,0 @@
-module registrador1b(input clock, input in, output out);
-	reg registrador1b;
-	
-	always @(posedge clock) begin
-		registrador1b <= in;
-	end
-	
-	assign out = registrador1b;
-endmodule
